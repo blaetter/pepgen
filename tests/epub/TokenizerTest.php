7a0d6efabe2635459310d698f7d46c1390c22e2f
Andreas Mängel <andreas.maengel@gmail.com>
2017-01-30 21:29:56 +0100

    added basic test for Tokenizer

---
+++ tests/epub/TokenizerTest.php
@@ -0,0 +1,20 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class TokenizeTest extends TestCase
+{
+
+    public function testTokenize()
+    {
+        $id = 'id';
+        $secret = 'secret';
+        $watermark = 'watermark';
+        $token = \Pepgen\helper\Tokenizer::tokenize($id, $secret, $watermark);
+        $this->assertEquals($token, md5(
+            $id.
+            $secret.
+            $watermark.
+            strftime("%d.%m.%Y")
+        ));
+    }
+}
