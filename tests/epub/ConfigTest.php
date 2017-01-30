ce0916a4f8ff72b1994c4c53ca1dfcf50806f014
Andreas Mängel <andreas.maengel@gmail.com>
2017-01-30 21:13:46 +0100

    added basic test for config

---
+++ tests/epub/ConfigTest.php
@@ -0,0 +1,12 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class ConfigTest extends TestCase
+{
+
+    public function testGetConfig()
+    {
+        $base_path = \Pepgen\helper\Config::get('base_path');
+        $this->assertEmpty($base_path);
+    }
+}
