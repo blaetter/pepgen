ae7b7a713ce9dd3ba02cf0b84863a0e9050be97e
ambo <andreas.maengel@gmail.com>
2019-11-03 15:40:06 +0100

    updated clear command:
    - added logs target to get rid of logs
    - added options for settings the number of days files will be kept
    - added a dry-run option
    - added tests for the above
    - added config option to set the log dir

--- app/modules/Command/ClearCommand.php
+++ app/modules/Command/ClearCommand.php
@@ -33,21 +33,44 @@ class ClearCommand extends Command
 
             // the full command description shown when running the command with
             // the "--help" option
-            ->setHelp("This command clears either the temp or the public epub folder.\nPer default it does not delete files that are less than 7 days old, but you might force deleting all files with --all.\nThis is mainly because we want to keep public ebooks for a litte bit to give users the chance to download it.\nThe temp folder should be empty as the temp files are deleted after successfully creating a personalized epub.")
+            ->setHelp(
+                "This command clears either the temp, public epub or log folder." . PHP_EOL .
+                "Per default it does not delete files that are less than 7 days old, but you might force deleting " .
+                "all files with --all or specify a number of days that needs to be kept with --days = 5." . PHP_EOL .
+                "This is mainly because we want to keep public ebooks for a litte bit to give users the chance " .
+                "to download it." . PHP_EOL .
+                "The temp folder should be empty as the temp files are deleted after successfully creating" .
+                "a personalized epub."
+            )
 
             ->addArgument(
                 'target',
                 InputArgument::REQUIRED,
-                'Which files should be deleted (temp|public)?'
+                'Which files should be deleted (temp|public|logs)?'
             )
 
-            // add an option to
+            // add an option to delete all
             ->addOption(
                 'all',
                 'a',
                 InputOption::VALUE_NONE,
                 'Should all files be deleted or only older ones?',
                 null
+            )
+            // add an option to set the day parameter
+            ->addOption(
+                'days',
+                'd',
+                InputOption::VALUE_REQUIRED,
+                'Override the number of days where files will kept. (Usually 7 days). ' .
+                'Option is ignored if --all is used. Option should be greater than 0.'
+            )
+            // add an option for dry run
+            ->addOption(
+                'dry-run',
+                null,
+                InputOption::VALUE_NONE,
+                'Dry run, so no actual file is deleted. Use this to check if you have old files.'
             );
         ;
     }
@@ -56,24 +79,52 @@ class ClearCommand extends Command
     {
         $filesystem = new Filesystem();
         $finder = new Finder();
+        $file_identifier = '*.epub';
+        // set the directory depending on the given target
         if ('temp' == $input->getArgument('target')) {
             $target_dir = $this->config->get('base_path') . $this->config->get('epub_temp_dir');
         } elseif ('public' == $input->getArgument('target')) {
             $target_dir = $this->config->get('base_path') . $this->config->get('epub_public_dir');
-        }
-        if ($input->getOption('all')) {
-            $finder->files()->name('*.epub');
+        } elseif ('logs' == $input->getArgument('target')) {
+            $target_dir = $this->config->get('base_path') . $this->config->get('epub_log_dir');
+            $file_identifier = '*.log';
         } else {
-            $finder->files()->name('*.epub')->date('< 7 days ago');
+            $output->writeln(
+                'Argument should be one of temp, public or logs'
+            );
+            return false;
         }
+
+        // decide, which files should be deleted.
+        if ($input->getOption('all')) {
+            $finder->files()->name($file_identifier);
+        } elseif ($input->getOption('days') && 0 < $input->getOption('days')) {
+            $finder->files()->name($file_identifier)->date('< ' . $input->getOption('days') . ' days ago');
+        } else {
+            $finder->files()->name($file_identifier)->date('< 7 days ago');
+        }
+
         if ($filesystem->exists($target_dir)) {
-            try {
-                $filesystem->remove($finder->in($target_dir));
-            } catch (IOException $e) {
-                $output->writeln(
-                    'Something went wrong: ' . $e->getMessage(),
-                    OutputInterface::VERBOSITY_VERBOSE
-                );
+            // set the finder to the actually wanted files
+            $finder->in($target_dir);
+
+            // check for dry-run
+            if ($input->getOption('dry-run')) {
+                // in dry-run, only display the files
+                $output->writeln('dry-run, printing files that matches given criteria');
+                foreach ($finder as $file) {
+                    $output->writeln($file);
+                }
+            } else {
+                // delete the files, if no dry-run is specified.
+                try {
+                    $filesystem->remove($finder->in($target_dir));
+                } catch (IOException $e) {
+                    $output->writeln(
+                        'Something went wrong: ' . $e->getMessage(),
+                        OutputInterface::VERBOSITY_VERBOSE
+                    );
+                }
             }
         }
     }
--- tests/Command/ClearCommandTest.php
+++ tests/Command/ClearCommandTest.php
@@ -14,6 +14,7 @@ class ClearCommandTest extends BaseTest
     private $filesystem;
     private $temp_dir;
     private $public_dir;
+    private $log_dir;
 
     protected function setUp(): void
     {
@@ -21,12 +22,80 @@ class ClearCommandTest extends BaseTest
         $this->config = new \Pepgen\Helper\Config();
         $this->filesystem = new Filesystem();
         $old_timestamp = strtotime('8 days ago');
+        $really_old_timestamp = strtotime('10 days ago');
+
+        // set some temp files for the temp dir
         $this->temp_dir = $this->config->get('base_path') . $this->config->get('epub_temp_dir');
         $this->filesystem->touch($this->temp_dir . '/test_new.epub');
         $this->filesystem->touch($this->temp_dir . '/test_old.epub', $old_timestamp);
+        $this->filesystem->touch($this->temp_dir . '/test_really_old.epub', $really_old_timestamp);
+
+        // set some temp files for the public dir
         $this->public_dir = $this->config->get('base_path') . $this->config->get('epub_public_dir');
         $this->filesystem->touch($this->public_dir . '/test_new.epub');
         $this->filesystem->touch($this->public_dir . '/test_old.epub', $old_timestamp);
+
+        // set some temp files for the log dir
+        $this->log_dir = $this->config->get('base_path') . $this->config->get('epub_log_dir');
+        $this->filesystem->touch($this->log_dir . '/test_new.log');
+        $this->filesystem->touch($this->log_dir . '/test_old.log', $old_timestamp);
+    }
+
+    public function testExecuteDeleteTempDryRun()
+    {
+        $application = new Application();
+        $application->add(new ClearCommand());
+
+        $command = $application->find('clear');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array(
+            'command'   => $command->getName(),
+            'target'    => 'temp',
+            '--dry-run'   => true,
+        ));
+        $this->assertTrue(
+            $this->filesystem->exists($this->temp_dir . '/test_new.epub') &&
+            $this->filesystem->exists($this->temp_dir . '/test_old.epub') &&
+            $this->filesystem->exists($this->temp_dir . '/test_really_old.epub')
+        );
+    }
+
+    public function testExecuteDeleteTempDaysOlder()
+    {
+        $application = new Application();
+        $application->add(new ClearCommand());
+
+        $command = $application->find('clear');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array(
+            'command'   => $command->getName(),
+            'target'    => 'temp',
+            '--days'      => 12,
+        ));
+        $this->assertTrue(
+            $this->filesystem->exists($this->temp_dir . '/test_new.epub') &&
+            $this->filesystem->exists($this->temp_dir . '/test_old.epub') &&
+            $this->filesystem->exists($this->temp_dir . '/test_really_old.epub')
+        );
+    }
+
+    public function testExecuteDeleteTempDaysNewer()
+    {
+        $application = new Application();
+        $application->add(new ClearCommand());
+
+        $command = $application->find('clear');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array(
+            'command'   => $command->getName(),
+            'target'    => 'temp',
+            '--days'      => 9,
+        ));
+        $this->assertTrue(
+            $this->filesystem->exists($this->temp_dir . '/test_new.epub') &&
+            $this->filesystem->exists($this->temp_dir . '/test_old.epub') &&
+            !$this->filesystem->exists($this->temp_dir . '/test_really_old.epub')
+        );
     }
 
     public function testExecuteDeleteTemp()
@@ -94,6 +163,38 @@ class ClearCommandTest extends BaseTest
         $this->assertFalse($this->filesystem->exists($this->public_dir . '/test_new.epub'));
     }
 
+    public function testExecuteDeleteLogs()
+    {
+        $application = new Application();
+        $application->add(new ClearCommand());
+
+        $command = $application->find('clear');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array(
+            'command'   => $command->getName(),
+            'target'    => 'logs',
+        ));
+        $this->assertTrue(
+            $this->filesystem->exists($this->log_dir . '/test_new.log') &&
+            !$this->filesystem->exists($this->log_dir . '/test_old.log')
+        );
+    }
+
+    public function testExecuteDeleteAllLogs()
+    {
+        $application = new Application();
+        $application->add(new ClearCommand());
+
+        $command = $application->find('clear');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array(
+            'command'   => $command->getName(),
+            'target'    => 'logs',
+            '--all'     => true,
+        ));
+        $this->assertFalse($this->filesystem->exists($this->log_dir . '/test_new.log'));
+    }
+
     protected function tearDown(): void
     {
         // In case there was something wrong with the tests, remove the files by hand.
@@ -101,6 +202,7 @@ class ClearCommandTest extends BaseTest
         $this->filesystem->remove($this->temp_dir . '/test_old.epub');
         $this->filesystem->remove($this->public_dir . '/test_new.epub');
         $this->filesystem->remove($this->public_dir . '/test_old.epub');
-
+        $this->filesystem->remove($this->log_dir . '/test_new.log');
+        $this->filesystem->remove($this->log_dir . '/test_old.log');
     }
 }
